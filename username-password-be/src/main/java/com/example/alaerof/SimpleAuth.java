83c9447449173091135ff88c9399f5c9550f45cc
Helen <helen_yrofeeva@epam.com>
2025-08-01 23:07:50 +0300

    J

--- username-password-be/src/main/java/com/example/alaerof/SimpleAuth.java
+++ username-password-be/src/main/java/com/example/alaerof/SimpleAuth.java
@@ -3,6 +3,7 @@ package com.example.alaerof;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// feature 3 J
 @SpringBootApplication
 public class SimpleAuth {
 
